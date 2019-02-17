b259c840979596caf79c463590d210dbe7c54ef5
Matt Coppola <matt.coppola@gmail.com>
2019-02-17 10:44:15 -0600

    code cleanup, styling, added grid.css

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+var diceImages = [
+    "<img src=assets/images/557px-Dice-1-b.svg.png />",
+    "<img src=assets/images/557px-Dice-2-b.svg.png />",
+    "<img src=assets/images/557px-Dice-3-b.svg.png />",
+    "<img src=assets/images/Dice-4-b.svg.png />",
+    "<img src=assets/images/Dice-5-b.svg.png />",
+    "<img src=assets/images/Dice-6-b.svg.png />",
+]
+
+var squares = document.querySelectorAll(".dice");
+
+$("button").click(anotherRoll);
+
+function anotherRoll() {
+    $(".totals").text("");
+    var total = 0
+    for (var i = 0; i < squares.length; i++) {
+        var die = randomRoll();
+        squares[i].innerHTML = diceImages[die];
+        total += die + 1;
+        $(".totals").append("Total of " + (i + 1) + " die is " + total + '<br>');
+    }
+}
+
+
+function randomRoll(){
+    var roll = Math.floor(Math.random() * 6);
+    return roll;
+}
+
+// KEEP - anotherRoll function without totals
+// function anotherRoll() {
+//     for (var i = 0; i < squares.length; i++) {
+//         squares[i].innerHTML = diceImages[randomRoll()];
+//     }
+// }
